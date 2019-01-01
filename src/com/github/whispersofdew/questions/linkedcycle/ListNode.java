42564449cfefae35df36e6b1ccaea93c2f7aa2da
alexey.korolev <A.Korolev@x5.ru>
2019-01-01 20:02:50 +0300

    Linked List Question

---
+++ src/com/github/whispersofdew/questions/linkedcycle/ListNode.java
@@ -0,0 +1,10 @@
+package com.github.whispersofdew.questions.linkedListCycle;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
