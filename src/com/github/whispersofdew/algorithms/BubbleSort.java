87ffbf8eff828f54aa1b55c44c22eac4135072b5
alexey.korolev <A.Korolev@x5.ru>
2019-01-02 20:49:37 +0300

    BubbleSort

--- src/com/github/whispersofdew/algorithms/BubbleSort.java
+++ src/com/github/whispersofdew/algorithms/BubbleSort.java
@@ -2,12 +2,42 @@ package com.github.whispersofdew.algorithms;
 
 public class BubbleSort {
 
-    int[] arrayOfInts = {9,5,3,1114,2,1};
-
     public static void main(String[] args) {
+
+        int intArray[] = new int[]{5,90,35,45,150,3};
+
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i < intArray.length; i++){
+            System.out.print(intArray[i] + " ");
+        }
+
+        bubbleSort(intArray);
+
+        System.out.println("");
+
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < intArray.length; i++){
+            System.out.print(intArray[i] + " ");
+        }
+
     }
 
-    void sort(int[] arrayOfInts){
+    private static void bubbleSort(int[] intArray) {
+
+        int n = intArray.length;
+        int temp = 0;
+
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+
+                if(intArray[j-1] > intArray[j]){
+                    temp = intArray[j-1];
+                    intArray[j-1] = intArray[j];
+                    intArray[j] = temp;
+                }
+
+            }
+        }
 
     }
 }
