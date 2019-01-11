ec5104f2b3dd35ea29ac4521a78d5e17a00cd521
alexey.korolev <whispersofdew@gmail.com>
2019-01-11 09:33:41 +0300

    kth Largest Element in Array question

--- src/com/github/whispersofdew/questions/largestelement/KthLargestElementInAnArray.java
+++ src/com/github/whispersofdew/questions/largestelement/KthLargestElementInAnArray.java
@@ -14,9 +14,7 @@ public class KthLargestElementInAnArray {
 
     public int findKthLargest(int[] nums, int k) {
         int length = nums.length;
-
         Arrays.sort(nums);
-
         return nums[length - k];
     }
 }
